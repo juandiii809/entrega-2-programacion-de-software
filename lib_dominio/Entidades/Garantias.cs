d8ddf7aa24d377323b7d2e3a7d3714ff209711da
juan <juanosorio1124226@correo.itm.edu.co>
2025-09-13 12:43:53 -0500

    Agregar archivos de proyecto.

---
+++ lib_dominio/Entidades/Garantias.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib_dominio.Entidades
+{
+    public class Garantias
+    {
+        public int Id { get; set; }
+        public DateTime Fecha_inicio { get; set; }
+        public DateTime Fecha_fin { get; set; }
+
+    }
+}
---
+++ lib_dominio/Entidades/Pagos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib_dominio.Entidades
+{
+    public class Pagos
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Monto { get; set; }
+        public string? Tipo_pago { get; set; }
+    }
+}
