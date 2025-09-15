e25a0b2f6934638f3a173dd3e395a26721a88b6b
SamuelC107 <samuelcorrea1116986@correo.itm.edu.co>
2025-09-15 16:25:41 -0500

    Clase Clientes

--- lib_dominio/Entidades/Clientes.cs
+++ lib_dominio/Entidades/Clientes.cs
@@ -10,5 +10,12 @@ namespace lib_dominio.Entidades
     {
         public int Id { get; set; }
         public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? Cedula { get; set; }
+        public string? Correo { get; set; }
+        public int Computador { get; set; }
+        public Computadores? Computadores { get; set; }
+
+        public List<Orden_Servicios>? Orden_Servicios;
     }
 }
