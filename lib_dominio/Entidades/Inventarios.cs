6eb626be756c865ca68e0f21e374979f2a73763a
SamuelC107 <samuelcorrea1116986@correo.itm.edu.co>
2025-09-15 16:39:08 -0500

    Clase Inventarios

--- lib_dominio/Entidades/Inventarios.cs
+++ lib_dominio/Entidades/Inventarios.cs
@@ -8,5 +8,10 @@ namespace lib_dominio.Entidades
 {
     public class Inventarios
     {
+        public int Id { get; set; }
+        public string? Descripcion { get; set; }
+        public int Piezas_Disponibles { get; set; }
+        public int Producto { get; set; }
+        public Productos? Productos { get; set; }
     }
 }
