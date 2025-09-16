b1803745d432b8487bac856073f2990f1ab75f3d
juan <juanosorio1124226@correo.itm.edu.co>
2025-09-15 23:47:02 -0500

    proy

---
+++ ut_presentacion/Nucleo/ComputadoresNucleo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib_dominio.Entidades;
+
+namespace ut_presentacion.Nucleo
+{
+    public class ComputadoresNucleo
+    {
+        public static Computadores? computadores()
+        {
+            var entidad = new Computadores();
+            entidad.Nombre = "ttp123";
+            entidad.Modelo = "ndeid";
+            entidad.Precio = 2000000.0m;
+            entidad.Marca = 1;
+            entidad.Componente = 1;
+            return entidad;
+        }
+    }
+}
---
+++ ut_presentacion/Nucleo/PagosNucleo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib_dominio.Entidades;
+
+namespace ut_presentacion.Nucleo
+{
+    public class PagosNucleo
+    {
+        public static Pagos? Pagos()
+        {
+            var entidad = new Pagos();
+            entidad.Fecha = DateTime.Now;
+            entidad.Monto = 1200000.0m;
+            entidad.Tipo_Pago = "tarjeta";
+            return entidad;
+        }
+    }
+}
